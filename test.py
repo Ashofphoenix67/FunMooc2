3bc211927d9e4b3cd4fca88b4dcfa15657a7afbd
Ashofphoenix <ashofphoenix@sunnox.fr>
2021-04-13 16:57:57 +0200

    -

---
+++ test.py
@@ -0,0 +1,12 @@
+def comptage(in_filename, out_filename):
+   "votre code"
+
+
+
+in_file ='lorem_ipsum.txt'
+out_file ='lorem_ipsum_out.txt'
+# in_file ='romeo_and_juliet.txt'
+# out_file ='romeo_and_juliet_out.txt'
+# in_file ='une_charogne_unicode.txt'
+# out_file ='une_charogne_unicode_out.txt'
+comptage(in_file, out_file)
